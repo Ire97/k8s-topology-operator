20e12e0d00744f04f74fe9908173edd725fe2d62
Angelo Marchese <marchese.angelo1@gmail.com>
2022-08-31 14:30:31 +0200

    Add telemetry service connector

---
+++ src/main/java/it/unict/telemetry/TelemetryService.java
@@ -0,0 +1,20 @@
+package it.unict.telemetry;
+
+import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import java.util.Map;
+
+@RegisterRestClient(configKey = "telemetry-api")
+public interface TelemetryService {
+
+    @GET
+    @Path("/metrics/node/avg-latencies")
+    Uni<Map<String,Float>> getAvgNodeLatencies(@QueryParam("node") String node);
+
+    @GET
+    @Path("/metrics/node/avg-latencies")
+    Uni<Map<String,Map<String,Float>>> getAllAvgNodeLatencies();
+}
